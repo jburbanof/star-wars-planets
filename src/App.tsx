8c4742edfa0ee7782a3a5f24180fb0c2b01e6475
Julián Burbano <jburbano64@gmail.com>
2022-03-24 00:36:34 -0500

    First commit & data fetching

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { useQuery } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
+import axios from "axios";
+
+interface resultTypes {
+	climate: string;
+	created: string;
+	diameter: string;
+	edited: string;
+	films: string[];
+	gravity: string;
+	name: string;
+	orbitalPeriod: string;
+	population: string;
+	redients: string[];
+	rotationPeriod: string;
+	surface_water: string;
+	terrain: string;
+	url: string;
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [planetsList, setPlanetsList] = useState([]);
+	const url = "https://swapi.dev/api/planets/";
+	const { isLoading, error, data } = useQuery("repoData", () =>
+		axios.get(url).then((res) => res.data)
+	);
+	useEffect(() => {
+		console.log(data);
+	}, [data]);
+
+	return (
+		<div className="App">
+			{data.results.map((planet: resultTypes) => {
+				return <h1>{planet.name}</h1>;
+			})}
+		</div>
+	);
 }
 
 export default App;
